d8f3c0deac9060c58a414b057ba1864d4b98ff4f
mariovzc <mario.vizcainos@gmail.com>
2020-09-20 01:48:59 -0500

    ADD: display links, delete data and redirect url

--- app.rb
+++ app.rb
@@ -14,19 +14,33 @@ get '/' do
   {status:'ok'}.to_json
 end
 
+get '/links' do
+  Link.all.map { |s| s.to_json }.to_json
+end
 
 post '/url_shortener' do
   params = JSON.parse request.body.read
-
-  url = params['url']
-
-  url_shortener = Link.new(url: url)
-
-  if url_shortener.save
-    url_shortener.to_json
-  else
-    status 400
-    body url_shortener.errors.to_json
+  link = Link.new(url: params['url'])
+  unless link.save
+    halt 400, link.errors.to_json
   end
+  status 201
+  {
+    shortened_url: link.slug
+  }.to_json
+end
 
+get '/:slug' do
+  link = Link.where(slug: params[:slug]).first
+  unless link
+    halt 404, { message: "Not found" }.to_json
+  end
+  link.set_clicked
+  redirect link.url
+end
+
+
+delete '/clean' do
+  Link.delete_all
+  {message:'db restored'}.to_json
 end
